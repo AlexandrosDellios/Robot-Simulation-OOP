82b86003741457e93c380fd8ca46788db18815fd
AlexandrosDellios <alexandrosdellios@icloud.com>
2023-04-22 17:40:40 +0200

    Graphic
    Graphic avec les dexu fonction pour dessiner les shapes et les palettes de couleurs

--- Code/graphic.cc
+++ Code/graphic.cc
@@ -1,1 +1,38 @@
+#include <cmath>
+#include "graphic_gui.h"
+
+
+static const Cairo::RefPtr<Cairo::Context>* ptcr(nullptr); 
+
+void graphic_set_context(const Cairo::RefPtr<Cairo::Context>& cr)
+{
+	ptcr = &cr;
+}
+
+void graphic_draw_square(const double cote, double xc, double yc, RGB color, 
+bool fill,RGB colorb) 
+{
+
+	(*ptcr)->rectangle(xc-(cote/2),yc-(cote/2),cote,cote);
+	if (fill==true){
+		(*ptcr)->set_source_rgb(colorb.r,colorb.g,colorb.b);
+		(*ptcr)->fill_preserve();
+	}
+	(*ptcr)->set_line_width(5.0);
+	(*ptcr)->set_source_rgb(color.r,color.g,color.b);
+	(*ptcr)->stroke();
+}
+
+void graphic_draw_circle(const double rayon, double xc, double yc, RGB color, 
+bool fill,RGB colorb,bool robotNeut)
+{
+	(*ptcr)->arc(xc,yc,rayon,0.0, 2*M_PI);
+	if (fill==true){
+		(*ptcr)->set_source_rgb(colorb.r,colorb.g,colorb.b);
+		(*ptcr)->fill_preserve();
+	}
+	(*ptcr)->set_line_width(5.0);
+	(*ptcr)->set_source_rgb(color.r,color.g,color.b);
+	(*ptcr)->stroke();
+}
 
--- Code/graphic.h
+++ Code/graphic.h
@@ -1,1 +1,22 @@
+#ifndef GTKMM_EXAMPLE_GRAPHIC_H
+#define GTKMM_EXAMPLE_GRAPHIC_H
+
+struct RGB{double r; double g; double b;};
+constexpr RGB WHITE = {1,1,1};
+constexpr RGB GREY = {0.5,0.5,0.5};
+constexpr RGB RED = {1,0,0};
+constexpr RGB LIGHT_BLUE = {0,0.5,1};
+constexpr RGB BLACK = {0,0,0};
+constexpr RGB PURPLE = {0.3,0,0.5};
+constexpr RGB ORANGE = {0.9,0.5,0.1};
+constexpr RGB GREEN = {0,0.5,0};
+
+constexpr bool FILLED (true);
+
+void graphic_draw_square(const double cote, double xc, double yc, RGB color, 
+bool fill,RGB colorb);
+void graphic_draw_circle(const double rayon, double xc, double yc, RGB color, 
+bool fill,RGB colorb,bool robotNeut);
+
+#endif // GTKMM_EXAMPLE_GRAPHIC_H
 
